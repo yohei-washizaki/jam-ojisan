c1d9c3cd6fa431e30b469e49812bc44e94eb5643
yohei-washizaki <yohei.washizaki@gmail.com>
2015-01-06 00:25:33 +0900

    カメラ揺らす機能を追加した。

---
+++ Assets/Editor/ShakerEditor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Shaker))]
+public class ShakerEditor : Editor {
+	public override void OnInspectorGUI ()
+	{
+		DrawDefaultInspector();
+
+		Shaker shaker = (Shaker)target;
+		if(GUILayout.Button("Shake"))
+		{
+			shaker.Shake();
+		}
+	}
+}
---
+++ Assets/Scripts/Behavior/Shaker.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[ExecuteInEditMode]
+public class Shaker : MonoBehaviour {
+	/// <summary>
+	/// The m shake sec.
+	/// </summary>
+	public float ShakeSec = 1.0f;
+
+	public List<AnimationCurve> Degrees = new List<AnimationCurve>(3);
+
+	/// <summary>
+	/// Shake this instance.
+	/// </summary>
+	public void Shake() {
+		_State 			= State.Shaking;
+		_ShakeSecTimer 	= this.ShakeSec;
+	}
+
+	enum State {
+		Idle,
+		Shaking,
+	};
+
+	State _State;
+	float _ShakeSecTimer;
+	Vector3 _Origin;
+	float normalizedTimer
+	{
+		get
+		{
+			if( this.ShakeSec <= 0 )
+				return 0;
+
+			return (this.ShakeSec - _ShakeSecTimer) / this.ShakeSec;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		_State = State.Idle;
+		_ShakeSecTimer = ShakeSec;
+		_Origin = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch( _State ){
+		case State.Idle:
+			break;
+		case State.Shaking:
+			execShake();
+			break;
+		}
+	}
+
+	void execShake(){
+		// update time
+		_ShakeSecTimer -= Time.deltaTime;
+
+		// update state
+		if( _ShakeSecTimer <= 0 ) {
+			_State = State.Idle;
+			transform.position = _Origin;
+			return;
+		}
+
+		// shake this instance
+		Vector3 degree = Random.insideUnitSphere * Degrees[0].Evaluate(1.0f - this.normalizedTimer);
+		this.transform.position = _Origin + degree;
+	}
+
+}
